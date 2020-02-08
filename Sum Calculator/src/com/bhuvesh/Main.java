a2822dde5803cbe0b03a0e7610f5f71a4efa7eb3
bhuveshaggarwal <bhuveshaggarwal@gmail.com>
2020-02-08 17:06:24 +0530

    My Simple Calculator Program

---
+++ Sum Calculator/src/com/bhuvesh/Main.java
@@ -0,0 +1,17 @@
+package com.bhuvesh;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(6.0);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
+    }
+}
